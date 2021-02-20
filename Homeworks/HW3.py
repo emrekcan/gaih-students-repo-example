62c1fb7ee0a8604bdd59462485d2841cd40187db
Emre <55927571+emrekcan@users.noreply.github.com>
2021-02-20 15:05:03 +0300

    Ödev 3 tamamlandı.

--- Homeworks/HW3.py
+++ Homeworks/HW3.py
@@ -1,5 +1,26 @@
-#Explain your work
+lowNum = 0
+upNum = 1000
 
-#Question 1
-for x in range(a):
-	print(a)
+def prime_first(num):
+    for prime_num in range(2, num + 1):
+        if prime_num > 1:
+            for i in range(2, prime_num):
+                if (prime_num % i) == 0:
+                    break
+            else:
+                print(prime_num)
+
+def prime_second(num):
+    for prime_num in range(2, num + 1):
+        if prime_num > 1:
+            for i in range(2, prime_num):
+                if (prime_num % i) == 0:
+                    break
+            else:
+                print(prime_num)
+
+for i in range(lowNum, upNum):
+    if i < 500:
+        prime_first(i)
+    if 500 < i < 100:
+        prime_second(i)
