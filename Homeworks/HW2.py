47bb5eaf2cf3782bfa4f044bbe6edb2a07cd0111
Emre <55927571+emrekcan@users.noreply.github.com>
2021-02-20 11:38:22 +0300

    Ödev 2 tamamlandı.

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,27 @@
-#Explain your work
+students = 5
+exams = 3
+name = 2
+grades = [[0 for i in range(exams)]for j in range(students)]
+names = [[0 for i in range(name)]for j in range(students)]
 
-#Question 1
-for x in range(a):
-	print(a)
+for i in range(students):
+    for j in range(exams):
+        grades[i][j] = int(input(f"{i+1}. öğrencinin sınav notlarını giriniz : \n"))
+    for k in range(name):
+        names[i][k] = input(f"{i+1}. öğrencinin ismini ve soy ismini giriniz : \n")
+
+info = dict()
+maxExam = 0
+bestStudent = tuple()
+
+for i in range(students):
+    examGrade = (grades[i][0] + grades[i][1] + grades[i][2]) // 3
+    studentsNames = (names[i][0], names[i][1])
+    info[(studentsNames)] = examGrade
+    if maxExam < examGrade:
+        maxExam = examGrade
+        bestStudent = (names[i][0], names[i][1])
+print(info)
+sortedInfo = sorted(info.values())
+print("Puan Listesi : ", sortedInfo)
+print("Tebrikler : ", bestStudent)
