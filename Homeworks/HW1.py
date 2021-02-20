bfa845d608320e3112ded6252a0a76d908b712f5
Emre <55927571+emrekcan@users.noreply.github.com>
2021-02-20 11:04:46 +0300

    Ödev 1 tamamlandı.

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,18 @@
-#Explain your work
+import random
 
-#Question 1
-for x in range(a):
-	print(a)
+upperNum = 100
+lowerNum = 2
+
+primeNumbers = list()
+
+for i in range(lowerNum, upperNum+1):
+    for j in range(lowerNum, i):
+        if(i%j == 0):
+            break
+        primeNumbers.append(i)
+#print(primeNumbers)
+matrix = [[0 for i in range(3)]for j in range(3)]
+for row in range(len(matrix)):
+    for column in range(len(matrix)):
+        matrix[row][column] = primeNumbers.pop(random.randint(0,len(primeNumbers)-1))
+print(matrix)
